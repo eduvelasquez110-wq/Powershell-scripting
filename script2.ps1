dd9996b63d75b2ff2a474e257edef2f6c6cda7c5
eduvelasquez110-wq <eduvelasquez110@gmail.com>
2025-12-09 18:49:23 -0500

    Update script2.ps1
    TRABAJO

--- script2.ps1
+++ script2.ps1
@@ -1,14 +1,14 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName System.Windows.Forms   #Carga las librerías, donde System.Windows.Form contiene las clases de los controles (Form, Button, TextBox, etc)
+Add-Type -AssemblyName System.Drawing      # System.Drawing contiene las clases para gestionar gráficos, colores y dimensiones
 
 # Create form
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Input Form"
-$form.Size = New-Object System.Drawing.Size(500,250)
-$form.StartPosition = "CenterScreen"
+$form = New-Object System.Windows.Forms.Form  #Crea la venatna donde estaran todos los controles 
+$form.Text = "Input Form"  #Establece el título    
+$form.Size = New-Object System.Drawing.Size(500,250)  #Establece el tamaño de la ventana 
+$form.StartPosition = "CenterScreen"      #Define la posición de la ventana, que en este caso aparecera centrada en la pantalla 
 
 ############# Define labels
-$textLabel1 = New-Object System.Windows.Forms.Label
+$textLabel1 = New-Object System.Windows.Forms.Label #Crea el objeto de Etiqueta 1
 $textLabel1.Text = "Input 1:"
 $textLabel1.Left = 20
 $textLabel1.Top = 20
@@ -27,7 +27,7 @@ $textLabel3.Top = 100
 $textLabel3.Width = 120
 
 ############# Textbox 1
-$textBox1 = New-Object System.Windows.Forms.TextBox
+$textBox1 = New-Object System.Windows.Forms.TextBox #Crea el objeto de Campo de Texto 1
 $textBox1.Left = 150
 $textBox1.Top = 20
 $textBox1.Width = 200
@@ -51,24 +51,24 @@ $textBox2.Text = $defaultValue
 $textBox3.Text = $defaultValue
 
 ############# OK Button
-$button = New-Object System.Windows.Forms.Button
+$button = New-Object System.Windows.Forms.Button   #Crea el boton "OK" con sus respectvas posiciones 
 $button.Left = 360
 $button.Top = 140
 $button.Width = 100
 $button.Text = "OK"
 
 ############# Button click event
-$button.Add_Click({
-    $form.Tag = @{
+$button.Add_Click({   #Se encarga de definir la acción que ocurrirá cuando el usuario haga clic en el botón
+    $form.Tag = @{  #Se almacena todo lo que se ingresa en el los recuadros de la GUI 
         Box1 = $textBox1.Text
         Box2 = $textBox2.Text
         Box3 = $textBox3.Text
     }
-    $form.Close()
+    $form.Close() #Al finalizar se cierra la ventana 
 })
 
 ############# Add controls
-$form.Controls.Add($button)
+$form.Controls.Add($button) #Añade todos los controles al formulario 
 $form.Controls.Add($textLabel1)
 $form.Controls.Add($textLabel2)
 $form.Controls.Add($textLabel3)
